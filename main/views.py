95f72b01552327e9d4eb7881a4952b2575a586d7
Gabriela Queiroz Santos de Souza <138119356+gabiqss@users.noreply.github.com>
2023-07-19 11:19:18 -0300

    Add files via upload

--- main/views.py
+++ main/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Carro
+
+def criar_carro(request):
+    if request.method == 'POST':
+        modelo = request.POST.get('modelo')
+        marca = request.POST.get('marca')
+        ano = request.POST.get('ano')
+        cor = request.POST.get('cor')
+
+
+        carro = Carro(modelo=modelo, marca=marca, ano=ano, cor=cor)
+        carro.save()
+
+        return redirect('forms-carros')
+    
+    else:
+        return render(request, 'main/index.html')
 
 # Create your views here.
